47b4ae6070165933dcc1efa89c254001b2abacb0
edsonha <edsonha@gmail.com>
2021-05-15 20:34:05 +0800

    Create GetActivities using Mediator

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,22 +11,16 @@ namespace API.Controllers
 {
   public class ActivitiesController : BaseApiController
   {
-    private readonly DataContext _context;
-    public ActivitiesController(DataContext context)
-    {
-      _context = context;
-    }
-
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-      return await _context.Activities.ToListAsync();
+      return await Mediator.Send(new List.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-      return await _context.Activities.FindAsync(id);
+      return Ok();
     }
   }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using Application.Activities;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -44,6 +46,7 @@ namespace API
           policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
         });
       });
+      services.AddMediatR(typeof(List.Handler).Assembly);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
