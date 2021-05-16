393b3d9d85d673ec2af53b8d83d07ba89a2d648f
edsonha <edsonha@gmail.com>
2021-05-16 21:46:46 +0800

    Refactor all the services in the startup.cs in the Extensions folder

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,3 @@
-using Application.Activities;
-using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore;
-using AutoMapper;
-using Application.Core;
-using Persistence;
+using API.Extensions;
 
 namespace API
 {
@@ -31,25 +25,8 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-
       services.AddControllers();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-      });
-      services.AddDbContext<DataContext>(opt =>
-      {
-        opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-      });
-      services.AddCors(opt =>
-      {
-        opt.AddPolicy("CorsPolicy", policy =>
-        {
-          policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-        });
-      });
-      services.AddMediatR(typeof(List.Handler).Assembly);
-      services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+      services.AddApplicationServices(_config);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
